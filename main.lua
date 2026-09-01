45ed25403215bde5bf7b55df77b8ea361519c8d3
DrakeHug <thanhhungsp30@gmail.com>
2026-09-01 20:10:19 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -37,15 +37,5 @@ local Main = Instance.new("Frame")
 Main.Name = "Main"
 Main.Size = UDim2.fromOffset(360, 190)
 Main.Position = UDim2.new(0.5, -180, 0.5, -95)
-Main.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
-Main.BorderSizePixel = 0
-Main.Parent = ScreenGui
-
-local MainCorner = Instance.new("UICorner")
-MainCorner.CornerRadius = UDim.new(0, 12)
-MainCorner.Parent = Main
-
-local Gradient = Instance.new("UIGradient")
-Gradient.Color = ColorSequence.new({
-	ColorSequenceKeypoint.new(0, Color3.fromRGB(0, 255, 150)),
+Main.BackgroundColor3 = Color3.fromRGB(20, 20
 ```
